b3016b7757de162b33c13cd6fce9fa9003ed42a0
Itzel Ventura <venit1123@gmail.com>
2019-04-22 13:02:57 -0700

    Connected App to server and tested connectivity to database

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
+  };
+
+  componentDidMount() {
+
+    fetch('/home')
+      .then((res) => {
+        return res.json();
+      })
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          })
+        }).catch(
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error: "Error: Unable to connect to DB"
+            })
+            console.log(error)
+          }
+        );
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      console.log(items);
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.firstname}>
+              {item.firstname}
+            </li>
+          ))}
+        </ul>
+      );
+    }
   }
 }
 
--- server/server.js
+++ server/server.js
@@ -1,8 +1,58 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
+const { Pool } = require('pg'); 
 
 let app = express();
+
+
+require('dotenv').config();
+
+app.use(express.json())
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL || `postgres://localhost:5432/tanda`,
+    ssl: process.env.NODE_ENV === 'production'
+});
+
+// app.get('/', async (req, res) => {
+//     res.send("Hello")
+// });
+
+
+app.get('/home', async (req, res) => {
+    let client;
+    
+    try {
+        client = await pool.connect();
+        let result = await client.query('SELECT * FROM participant');
+        console.log(result.rows);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server error");
+    }
+    finally { client && client.release(); }
+
+});
+
+
+
+
+
+
+
+
+
+if (process.env.NODE_ENV === "production") {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, "../client/build")));
+    // Handle React routing, return all requests to React app
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    });
+}
 
 const PORT = process.env.PORT || 3000;
 

2bfa1d859ba47532d0313021acfd29f54bbdf335
Itzel Ventura <venit1123@gmail.com>
2019-04-29 09:42:02 -0700

    Styled and added cards in home pages

---
+++ client/src/Card.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
+import './Welcome.css'
+import './App.css'
+
+class Card extends Component {
+  render() {
+    return (
+      <div className='container-fluid'>
+        <div className='container'>
+          <div className='row'>
+            <div className="card" >
+              <img src={this.props.image} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{this.props.title}</h5>
+                <p className="card-text">{this.props.description}</p>
+                {/* <a href="#" className="btn btn-primary">{this.props.button}</a> */}
+                <button onClick={() => this.props.history.push('/newTanda')}>
+                  {this.props.button}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withRouter(Card);
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './App.css'
+import Card from './Card.js'
+import saveImage from './images/saving.jpg'
+import givingImage from './images/giving.jpg'
+import moneyJar from './images/moneyJar.jpg'
 
 class Home extends Component {
-    nextPath(path) {
-        this.props.history.push(path);
-    }
-    
     render(){
         return(
             <div className="body container-fluid">
-                <div className="container">
                     <div className="row">
-                        <div>
-                             <button onClick = {() =>  this.nextPath('/newTanda')} >
-                            Create Tanda 
-                            </button>
-                            
-                    
-                            </div>
+                        <div className='col-sm-12 col-md-4'>
+                            <Card 
+                                title="Create new tanda"
+                                description="Create a new tanda and invite friends and family to be part of the saving circle."
+                                button="Get started"
+                                image={saveImage}
+                            />
+                        </div>
+                        <div className='col-sm-12 col-md-4'>
+                            <Card
+                                title="Current Tandas"
+                                description='Place holder paragraph'
+                                button="Get started"
+                                image={givingImage}
+                            />
+                        </div>
+                        <div className='col-sm-12 col-md-4'>
+                            <Card
+                                title="Your savings"
+                                description='Place holder paragraph'
+                                button="Get started"
+                                image={moneyJar}
+                            />
                         </div>
                     </div>
-                </div>
+            </div>
        )
     }
 }
